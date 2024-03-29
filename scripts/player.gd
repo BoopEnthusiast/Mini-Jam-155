99abd16ce971fee707051856cb2971677eec8edb
AJW8 <aaronjw32@gmail.com>
2024-03-29 18:58:58 +1300

    Now prints raycast collder if any

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,5 +1,6 @@
 extends CharacterBody3D
 
+@onready var ray_cast_3d = $Camera/RayCast3D
 
 const SPEED = 5.0
 const JUMP_VELOCITY = 4.5
@@ -46,19 +47,18 @@ func camera_look(movement: Vector2) -> void:
 	rotate_object_local(Vector3.UP, -camera_rotation.x)
 	main_camera.rotate_object_local(Vector3.RIGHT, -camera_rotation.y)
 
-
 func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y -= gravity * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var input_dir = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	var input_dir = Input.get_vector("left", "right", "forward", "backward")
 	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
 		velocity.x = direction.x * SPEED
@@ -68,3 +68,5 @@ func _physics_process(delta):
 		velocity.z = move_toward(velocity.z, 0, SPEED)
 
 	move_and_slide()
+	
+	print(ray_cast_3d.getcollider())
