1029aa6859b68b42d5707369fe828cf3ae7d653f
thomasmaddockss <tjmaddocks1@gmail.com>
2024-03-29 18:02:21 +1300

    wind area3d

---
+++ scenes/wind.gd
@@ -0,0 +1,15 @@
+extends Area3D
+
+var wind_direction = Vector3(1, 0, 0) # wind direction
+var wind_force = 10 # wind force
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	for body in get_overlapping_bodies():
+		var force = wind_force * wind_direction.normalized()
+		body.apply_impulse(Vector3.ZERO, force)
