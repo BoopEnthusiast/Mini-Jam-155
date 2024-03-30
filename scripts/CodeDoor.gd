c54f247f9e78bd335af2fd417f54a4a7b4b64ace
Boop <102631339+BoopEnthusiast@users.noreply.github.com>
2024-03-30 13:14:59 +1300

    door fix + some generic stuff + antialiasing fix

--- scripts/CodeDoor.gd
+++ scripts/CodeDoor.gd
@@ -6,28 +6,25 @@ var numbers: Array[int] = [0, 0, 0]
 @export var unlock_code: Array[int] = [1, 9, 2]
 
 # Get nodes
-@onready var animation_player = $AnimationPlayer
-@onready var number_1 = $Number1
-@onready var number_2 = $Number2
-@onready var number_3 = $Number3
+@onready var animation_player = $"../AnimationPlayer"
 
 
-func _on_number_1_number_changed(number):
+func _on_number_1_number_changed(number: int) -> void:
 	numbers[0] = number
-	if not open and numbers == unlock_code:
-		open = true
-		animation_player.play("open_door")
+	check_if_unlocked()
 
 
-func _on_number_2_number_changed(number):
+func _on_number_2_number_changed(number: int) -> void:
 	numbers[1] = number
-	if not open and numbers == unlock_code:
-		open = true
-		animation_player.play("open_door")
+	check_if_unlocked()
 
 
-func _on_number_3_number_changed(number):
+func _on_number_3_number_changed(number: int) -> void:
 	numbers[2] = number
+	check_if_unlocked()
+
+
+func check_if_unlocked() -> void:
 	if not open and numbers == unlock_code:
 		open = true
 		animation_player.play("open_door")
--- scripts/character_select.gd
+++ scripts/character_select.gd
@@ -4,9 +4,9 @@ var player_1_scene = preload("res://scenes/player_1_main.tscn")
 var player_2_scene = preload("res://scenes/player_2_main.tscn")
 
 
-func _on_button_pressed():
+func _on_button_pressed() -> void:
 	get_tree().change_scene_to_packed(player_1_scene)
 
 
-func _on_button_2_pressed():
+func _on_button_2_pressed() -> void:
 	get_tree().change_scene_to_packed(player_2_scene)
