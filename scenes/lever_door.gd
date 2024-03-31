1e41efac2c6128fe079fe53f3bfd896ac804040f
Boop <102631339+BoopEnthusiast@users.noreply.github.com>
2024-04-01 01:36:27 +1300

    color puzzle done

--- scenes/lever_door.gd
+++ scenes/lever_door.gd
@@ -5,7 +5,7 @@ var open = false
 @onready var door_animator = $DoorAnimator
 
 
-func _on_lever_set_open_door():
+func _on_lever_open_door():
 	if not open:
 		open = true
 		door_animator.play("open_doors")
