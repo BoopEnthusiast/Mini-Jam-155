d1d487789e85ff6791f7e4f6b79b5e9be5aff251
Boop <102631339+BoopEnthusiast@users.noreply.github.com>
2024-04-01 13:36:13 +1300

    Final puzzle

---
+++ scripts/character_animal.gd
@@ -0,0 +1,27 @@
+extends StaticBody3D
+class_name CharacterAnim
+
+const ALPHABET_LENGTH = 6
+const CHARACTER_MESH = preload("res://resources/character_mesh.tres")
+
+signal character_changed(character: String, which: Character)
+
+var alphabet: Array[String] = ['Lion', 'Snake', 'Monkey', 'Elephant', 'Bear', 'Antelope']
+var current_letter: String
+
+@onready var mesh = $Mesh
+
+
+func _ready():
+	mesh.mesh = CHARACTER_MESH.duplicate()
+	current_letter = alphabet.pick_random()
+	mesh.mesh.text = current_letter
+
+
+func interact() -> void:
+	if alphabet.find(current_letter) == ALPHABET_LENGTH - 1:
+		current_letter = alphabet[0]
+	else:
+		current_letter = alphabet[alphabet.find(current_letter) + 1]
+	mesh.mesh.text = current_letter
+	character_changed.emit(current_letter, self)
